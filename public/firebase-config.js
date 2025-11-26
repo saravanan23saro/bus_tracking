7fcd0e739f5eaa598400918e69fda500dd465810
saravanan23saro <sarajothi1234@gmail.com>
2025-11-26 14:28:59 +0530

    Add public files for hosting and vercel.json

---
+++ public/firebase-config.js
@@ -0,0 +1,11 @@
+/* Deployed Firebase config (copied from project root) */
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDPXYM2AdM8KFHVlAwVyBcn8802_hB64MA",
+  authDomain: "tracking-7473f.firebaseapp.com",
+  databaseURL: "https://tracking-7473f-default-rtdb.firebaseio.com",
+  projectId: "tracking-7473f",
+  storageBucket: "tracking-7473f.firebasestorage.app",
+  messagingSenderId: "1003471303440",
+  appId: "1:1003471303440:web:700440d7b1079b95506b95",
+  measurementId: "G-ZHNH6JYL1E"
+};
